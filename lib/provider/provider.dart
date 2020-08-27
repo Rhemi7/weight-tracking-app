251d163115441bdfa9bae3538a899ec43772a00b
Rhemi7 <adelefatai@yahoo.com>
2020-08-27 02:34:00 +0100

    Added database provider to list of providers

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,7 +1,9 @@
+import 'package:logisticstrackerapp/database/database.dart';
 import 'package:logisticstrackerapp/services/auth_services.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 final providers = <SingleChildWidget>[
   ChangeNotifierProvider(create: (_) => AuthServices()),
+  ChangeNotifierProvider(create: (_) => Database()),
 ];
